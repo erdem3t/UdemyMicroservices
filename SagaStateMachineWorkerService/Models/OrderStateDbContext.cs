6d2df17706810fd20c3b1462642a8d20757f69cd
erdem <erdemcosgunnn@gmail.com>
2022-09-17 18:22:18 +0300

    Düzenleme Yapıldı

---
+++ SagaStateMachineWorkerService/Models/OrderStateDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit.EntityFrameworkCoreIntegration;
+using MassTransit.EntityFrameworkCoreIntegration.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace SagaStateMachineWorkerService.Models
+{
+    public class OrderStateDbContext : SagaDbContext
+    {
+        public OrderStateDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations { get { yield return new OrderStateMap(); } }
+    }
+}
