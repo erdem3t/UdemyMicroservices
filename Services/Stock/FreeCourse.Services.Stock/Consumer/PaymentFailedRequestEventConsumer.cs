5867db0c5fd54089ac4597b573f17b6ccbc6e2e6
erdem <erdemcosgunnn@gmail.com>
2022-09-18 17:01:04 +0300

    Düzenleem Yapıldı

---
+++ Services/Stock/FreeCourse.Services.Stock/Consumer/PaymentFailedRequestEventConsumer.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.Events;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Stock.Consumer
+{
+    public class PaymentFailedRequestEventConsumer : IConsumer<PaymentFailedRequestStock>
+    {
+        public Task Consume(ConsumeContext<PaymentFailedRequestStock> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
