b6ec16d0ff020d3a9d56c54909a30c6c5c79e3b8
erdem <erdemcosgunnn@gmail.com>
2022-09-18 15:07:24 +0300

    Düzenleme Yapıldı

---
+++ Services/Payment/FreeCourse.Services.Payment/Consumers/StockReservedRequestPaymentConsumer.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.EventsContract;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Payment.Consumers
+{
+    public class StockReservedRequestPaymentConsumer : IConsumer<IStockReservedRequestPayment>
+    {
+        public Task Consume(ConsumeContext<IStockReservedRequestPayment> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
